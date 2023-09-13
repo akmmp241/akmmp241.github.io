a509f0246a4ccfada3a1b0b6701dc5576cb8d183
akmmp241 <akm.mp24@gmail.com>
2023-09-13 21:11:09 +0700

    fix projects container and about details

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,16 @@ function toggleMenu() {
   menuLinks.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const projectSection = document.querySelector('#projects');
+const projectContainer = document.querySelector('#project-container');
+
+window.addEventListener('resize', function () {
+  if (projectContainer.offsetHeight > window.innerHeight) {
+    projectSection.classList.add('h-fit');
+    projectSection.classList.remove('h-[100vh]');
+  } else {
+    projectSection.classList.remove('h-fit');
+    projectSection.classList.add('h-[100vh]');
+  }
+});
